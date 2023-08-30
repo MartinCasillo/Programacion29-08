27377c7edd6c132e49bda7841f83a659399983b0
Martin <casillomartin29@gmail.com>
2023-08-29 21:38:30 -0300

    tarea

---
+++ src/Adivinanza.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Adivinanza {
+    public static void main(String[] args){
+     /*   int numeroAdivinar = 5;
+        // Random.next
+        for (int i = 0; i < 3;i++){
+            int numeroAdivinar = 5;
+            Scanner scanner = new Scanner(System.in)
+            System.out.println("Adivine el numero : " );
+            if(numUsuario =)
+        }
+
+
+    }*/
+}
+}
